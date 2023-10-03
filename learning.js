56c8946f65306264ec06c89db994acd6e0793925
Olivia <osalvini1@live.maryville.edu>
2023-10-02 22:24:37 -0700

    Your commit message here

--- learning.js
+++ learning.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Learning is Repeated Exposure Over Time\n')
+   response.end('Learning is Repeated Exposure Over Time.\n')
 }).listen(4444)
 
 // Console will print the message
